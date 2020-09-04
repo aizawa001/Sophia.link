bdefafd0029a0e1f8bb70f70f9ddf5368c001750
aizawa001 <aizawakyou778@gmail.com>
2020-09-04 10:32:04 +0900

    Initial Commit

---
+++ SophiaLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SophiaLink
+//
+//  Created by 五十嵐慎吉 on 2020/09/04.
+//  Copyright © 2020 五十嵐慎吉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
