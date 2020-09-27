ff17a30b87211588b45842d5251cafccaa2bf7ef
aizawa001 <aizawakyou778@gmail.com>
2020-09-27 16:14:39 +0900

    getTimeSlotData,getCollegeClassDataを作成①

--- SophiaLink/Model/TimeSlotData.swift
+++ SophiaLink/Model/TimeSlotData.swift
@@ -7,16 +7,60 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseFireStore
+
 
 class TimeSlotData{
-    var c_0: DocumentReference?
-    var c_1: DocumentReference?
-    var c_2: DocumentReference?
-    var c_3: DocumentReference?
-    var c_4: DocumentReference?
-    var c_5: DocumentReference?
-    var c_6: DocumentReference?
-    var user_id: String?
-    var document_id: String?
+    var c_0: DocumentReference!
+    var c_1: DocumentReference!
+    var c_2: DocumentReference!
+    var c_3: DocumentReference!
+    var c_4: DocumentReference!
+    var c_5: DocumentReference!
+    var c_6: DocumentReference!
+    var user_id: String!
+    var documentId: String!
+    
+    init(c_0: DocumentReference,c_1: DocumentReference,c_2: DocumentReference,c_3: DocumentReference, c_4: DocumentReference,c_5: DocumentReference,c_6: DocumentReference,user_id: String,documentId : String){
+        self.c_0 = c_0
+        self.c_1 = c_1
+        self.c_2 = c_2
+        self.c_3 = c_3
+        self.c_4 = c_4
+        self.c_5 = c_5
+        self.c_6 = c_6
+        self.user_id = user_id
+        self.documentId = documentId
+    }
+    
+    //配列を作る
+    var collegeClassDatas = [CollegeClassData]()
+    
+    //CollegeClassDataを取り出す
+    func getCollegeClassData(from ref: DocumentReference, completion : @escaping ((CollegeClassData?) -> Void)) {
+        
+        //データを取得
+        db.collection("collegeClassData").getDocuments(){(snapshot, error)in
+            //もしエラーならやめる
+            if let error = error{
+                print(error)
+            }
+            
+            //optional変数であるため"guard let"
+            guard let snap = snapshot else { return }
+            //snapから取り出す
+            for document in snap.documents{
+                
+                let subject = document["subject"] as! String
+                let professor = document["professor"] as! String
+                let classroom = document["classroom"] as! String
+                let documentId = document.documentID
+                
+                let newCollegeClass = CollegeClassData(classroom: classroom, professor: professor, subject: subject, documentId: documentId)
+                
+                self.collegeClassDatas.append(newCollegeClass)
+            }
+        }
+        
+    }
 }
--- SophiaLink/Model/UserData.swift
+++ SophiaLink/Model/UserData.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseFirestore
 
 class UserData {
     var name: String!
@@ -28,5 +28,41 @@ class UserData {
         self.friday = friday
         self.saturday = saturday
         self.documentId = documentId
+    }
+    
+    let db = Firestore.firestore()
+    //配列を作る
+    var timeSlotDatas = [TimeSlotData]()
+    
+    //TimeSlotDataを取り出す
+    func getTimeSlotData(from ref: DocumentReference, completion : @escaping ((TimeSlotData?) -> Void)){
+        
+        //データを取得
+        db.collection("timeSlotData").getDocuments(){(snapshot, error)in
+            //もしエラーならやめる
+            if let error = error{
+                print(error)
+            }
+            
+            //optional変数であるため"guard let"
+            guard let snap = snapshot else { return }
+            //snapから取り出す
+            for document in snap.documents{
+                
+                let c_0 = document["c_0"] as! String
+                let c_1 = document["c_1"] as! String
+                let c_2 = document["c_2"] as! String
+                let c_3 = document["c_3"] as! String
+                let c_4 = document["c_4"] as! String
+                let c_5 = document["c_5"] as! String
+                let c_6 = document["c_6"] as! String
+                let user_id = document["user_id"] as! String
+                let documentId = document.documentID
+                
+                let newTimeSlot = timeSlotDatas(c_0: c_0,c_1: c_1,c_2: c_2,c_3: c_3,c_4: c_4,c_5: c_5,c_6: c_6,user_id: user_id,documentId: documentId)
+                
+                self.timeSlotDatas.append(newTimeSlot)
+            }
+        }
     }
 }
