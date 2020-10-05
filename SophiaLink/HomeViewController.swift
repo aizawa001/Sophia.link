7bb61ad3ef1a053796f403799ee54a3c4118a99a
hideyasu2015 <hideyasu.tokuhara@gmail.com>
2020-10-05 19:37:59 +0900

    add sample

--- SophiaLink/HomeViewController.swift
+++ SophiaLink/HomeViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 import FirebaseFirestore
+import Firebase
 
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     let db = Firestore.firestore()
     
-    
+    //ログイン状態の保持
+    private var handle: AuthStateDidChangeListenerHandle?
+//詰め込みたい配列が必要です。
+
+
     
     @IBOutlet weak var timeTableCllectionView: UICollectionView!
     
@@ -26,7 +31,37 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         // Do any additional setup after loading the view.
         
     }
-    
+
+    override func viewWillAppear(_ animated: Bool) {
+        //こんなイメージです
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            if user == nil{
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let loginVC = storyboard.instantiateViewController(withIdentifier: "ログインさせたいページ")
+                self.present(loginVC, animated: true)
+            } else {
+//ログイン監視する関数を呼ぶ
+//                self.setListener()
+            }
+        })
+
+    }
+
+
+////監視する関数
+//    func setListener() {
+//必要なコレクションのスナップショットを取得
+//                .addSnapshotListener({ (snapshot, err) in
+//                    if let error = err{
+//                        print(error)
+//                    }else{
+//                        配列を削除して
+//                        配列にsnapshotでdata取得して
+//                        reloadします。
+
+
+//    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 42 // 表示するセルの数
     }
